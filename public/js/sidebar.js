93d8105867ed06f26dd5afbc8bf19608ce4623f4
donsemas <donsemas@gmail.com>
2026-09-08 10:17:15 +0300

    feat: маска телефона +7 (___) ___-__-__ в форме замера

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -22,8 +22,28 @@
     calc();
   }
 
+  function maskPhone(input) {
+    // Маска +7 (___) ___-__-__: цифры группируются при вводе, формат виден сразу
+    input.addEventListener("input", function () {
+      var d = input.value.replace(/\D/g, "");
+      if (d.charAt(0) === "8") d = "7" + d.slice(1);
+      if (d.charAt(0) !== "7") d = "7" + d;
+      d = d.slice(0, 11);
+      if (d.length <= 1) {
+        input.value = "";
+        return;
+      }
+      var out = "+7 (" + d.slice(1, 4);
+      if (d.length >= 4) out += ") " + d.slice(4, 7);
+      if (d.length >= 7) out += "-" + d.slice(7, 9);
+      if (d.length >= 9) out += "-" + d.slice(9, 11);
+      input.value = out;
+    });
+  }
+
   function initMeasure(root) {
     var phone = root.querySelector("[data-phone]");
+    maskPhone(phone);
     root.querySelector("[data-call]").addEventListener("click", function () {
       var msg = "Здравствуйте! Вызовите замерщика, мой телефон: " +
         (phone.value.trim() || "перезвоните мне") + ". 33navesa.ru";
